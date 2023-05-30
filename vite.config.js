4b5808a1048d018b42831312043607802172be0f
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-05-30 12:15:03 +0100

    sass import config solved

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import sass from "sass";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import './src/sass/config.scss';
+        `,
+        implementation: sass,
+      },
+    },
+  },
+});
