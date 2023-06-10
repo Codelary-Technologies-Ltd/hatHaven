21dc1b4f06165b377e97a34962328e4a7f87d45c
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-06-10 14:58:15 +0100

    feat: created the category header jsx

---
+++ src/components/CategoryHeader.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const CategoryHeader = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <p>Home/BucketHat</p>
+        </nav>
+        <div>
+            <h1>BUCKET HAT</h1>
+            <p>
+                Stay cool and stylish this season with our trendy bucket hat
+                collection. Whether you're headed to the beach or just running
+                errands around town, our bucket hats will keep you looking and
+                feeling great. Shop now and upgrade your hat game.
+            </p>
+        </div>
+        <nav>
+            <ul>
+                <li>CATEGORY</li>
+                <li>FILTER</li>
+                <li>SORT BY</li>
+            </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default CategoryHeader;
