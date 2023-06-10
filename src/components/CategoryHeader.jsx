d90d43409cd8c800ee1b1f3dfcceaca9b7982e81
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-06-10 15:32:53 +0100

    feat: created the categoryHeader

--- src/components/CategoryHeader.jsx
+++ src/components/CategoryHeader.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-
+import style from "./../sass/components/categoryHeader.module.scss";
 const CategoryHeader = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <p>Home/BucketHat</p>
-        </nav>
-        <div>
-            <h1>BUCKET HAT</h1>
-            <p>
+    <header className="container">
+        <div className={style.categoryHeader}>
+            <nav className={style.categoryHeaderTopNav}>
+                <p>Home/ BucketHat</p>
+            </nav>
+            <div className={style.categoryHeaderContent}>
+                <h1 className={style.categoryHeaderContentHeader}>BUCKET HAT</h1>
+                <p className={style.categoryHeaderContentDetails}>
                 Stay cool and stylish this season with our trendy bucket hat
-                collection. Whether you're headed to the beach or just running
-                errands around town, our bucket hats will keep you looking and
-                feeling great. Shop now and upgrade your hat game.
-            </p>
+                collection. Whether you're headed to the beach or just running errands
+                around town, our bucket hats will keep you looking and feeling great.
+                Shop now and upgrade your hat game.
+                </p>
+            </div>
+            <nav className={style.categoryHeaderBottomNav}>
+                <ul>
+                    <li>CATEGORY</li>
+                    <li className={style.categoryHeaderBottomNavFilter}>FILTER</li>
+                    <li>SORT BY</li>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul>
-                <li>CATEGORY</li>
-                <li>FILTER</li>
-                <li>SORT BY</li>
-            </ul>
-        </nav>
-      </header>
-    </div>
+    </header>
   );
 };
 
