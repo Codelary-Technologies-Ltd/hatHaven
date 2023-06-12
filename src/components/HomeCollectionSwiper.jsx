b122d52632be6d5e2ae865d9ad32c85e23895c6f
Hameedarh <ajibadeolayimika@gmail.com>
2023-06-12 22:00:30 +0100

    chore: review correction

--- src/components/HomeCollectionSwiper.jsx
+++ src/components/HomeCollectionSwiper.jsx
@@ -4,14 +4,7 @@ import SwiperCore, { Navigation, Pagination, EffectFade, Autoplay } from "swiper
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
-import pic from '../assets/png/categories-of -hats/beanie/beanie.jpg'
-import pic2 from '../assets/png/categories-of -hats/Balaclava hat/balaclava.jpg'
-import pic3 from '../assets/png/categories-of -hats/fedora/fedora.jpg'
-import pic4 from '../assets/png/categories-of -hats/straw-hat/strawhat.jpg'
-import pic5 from '../assets/png/categories-of -hats/bucket hat/bucket hat.jpg'
-import pic6 from '../assets/png/categories-of -hats/cowboy-hat/cowboy-4.jpg'
-import pic7 from '../assets/png/categories-of -hats/bucket hat/buckethat7.jpg'
-
+import { pictures } from './picture';
 
 function HomeCollectionSwiper() {
     SwiperCore.use([Navigation, Pagination, EffectFade, Autoplay]);
@@ -34,27 +27,17 @@ function HomeCollectionSwiper() {
                 // pagination={{ clickable: true }}
                 className="mySwiper"
                 >
-                <SwiperSlide className={styles2.collections}>
-                    <img src={pic} style={{width: '200px', height: '250px'}}/>
+                {pictures.map((slide, index) => (
+                <SwiperSlide 
+                key={slide.id ? slide.id : index}
+                className={styles2.collections}
+                >
+                <img 
+                    src={slide?.thumbnail}
+                    alt={slide?.title}
+                />
                 </SwiperSlide>
-                <SwiperSlide>
-                    <img src={pic2} style={{width: '200px', height: '250px'}}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={pic3} style={{width: '200px', height: '250px'}}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={pic4} style={{width: '200px', height: '250px'}}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={pic5} style={{width: '200px', height: '250px'}}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={pic6} style={{width: '200px', height: '250px'}}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={pic7} style={{width: '200px', height: '250px'}}/>
-                </SwiperSlide>
+                ))}
             </Swiper>
             </section>
         </div>
