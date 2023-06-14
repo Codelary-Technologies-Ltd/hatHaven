115c9df85510d278833b9400b86affca17fd3c96
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-06-14 19:27:26 +0100

    feat: created the category page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Navbar from "./../components/Navbar"
-import Footer from "./../components/Footer"
+
 import HomePageHeader from "./../components/HomePageHeader"
 import HomeCollection from "./../components/HomeCollection"
 import HomePageTestimonials from "./../components/HomePageTestimonial"
