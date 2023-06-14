b336f839b9749167eb4fdd9dea007b6c5c2f4479
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-06-14 19:37:24 +0100

    feat: created the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import style from './App.module.scss'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import Category from "./pages/Category"
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 function App() {
 
   return (
     <>
       <div>
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />}/>    
+            <Route path='/category' element={<Category />}/>     
+          </Routes>
+        </BrowserRouter>
+        <Footer />
       </div>
     </>
   )
