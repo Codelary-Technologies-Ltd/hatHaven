9d1e1d17402dc23e6555fec4e8e8ff02ee534e73
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-06-18 20:44:39 +0100

    chore: added the route for about us page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import style from './App.module.scss'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import Category from "./pages/Category"
+import AboutUs from './pages/AboutUs';
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}/>    
             <Route path='/category' element={<Category />}/>     
+            <Route path='/about-us' element={<AboutUs />}/>     
           </Routes>
         </BrowserRouter>
         <Footer />
