347289ef19e53b12dc60c47d119442567e87acfa
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-06-19 10:32:26 +0100

    feat: added the svgr plugi for svg component

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import counterReducer from "./features/counterSlice"
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
