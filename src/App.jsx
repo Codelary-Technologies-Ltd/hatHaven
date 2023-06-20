ba2f6dac4a265b1be31c6f475d654fa986fb79c6
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-06-20 01:12:57 +0100

    feat: added the shoppig cart routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import Category from "./pages/Category"
 import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
+import ShoppingCart from './pages/ShoppingCart'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}/>    
             <Route path='/category' element={<Category />}/>     
+            <Route path='/shopping-cart' element={<ShoppingCart />}/>     
             <Route path='/about-us' element={<AboutUs />}/>     
             <Route path='/contact-us' element={<ContactUs />}/>     
           </Routes>
