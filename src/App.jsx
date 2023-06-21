900e73dcc8bbf473c033c1c056306016623518b7
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-06-21 11:35:41 +0100

    feat: all correction made

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
 import ShoppingCart from './pages/ShoppingCart'
 import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 function App() {
@@ -18,10 +19,11 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}/>    
             <Route path='/sign-up' element={<SignUp />}/>    
+            <Route path='/login' element={<Login />}/>    
             <Route path='/category' element={<Category />}/>     
             <Route path='/shopping-cart' element={<ShoppingCart />}/>     
             <Route path='/about-us' element={<AboutUs />}/>     
-            <Route path='/contact-us' element={<ContactUs />}/>     
+            <Route path='/contact-us' element={<ContactUs />}/>
           </Routes>
         </BrowserRouter>
         <Footer />
