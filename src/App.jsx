633e4585cbf68ee4acacadcab669d616569bd2eb
Achilihu Daniel Uchenna <achilihudaniel53@gmail.com>
2023-07-09 17:29:28 +0100

    chore: moving categoryitem from components to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import style from './App.module.scss'
+// import style from './App.module.scss'
+import CategoryItem from './pages/CategoryItem.jsx'
 function App() {
 
   return (
     <>
       <div>
+        <CategoryItem/>
       </div>
     </>
   )
