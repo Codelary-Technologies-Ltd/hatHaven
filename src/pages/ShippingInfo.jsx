4b3b90c8f7ccc98d63648fade61eabbad14ba5b1
Hameedarh <ajibadeolayimika@gmail.com>
2023-07-19 15:38:59 +0100

    chore: corrected reviews

--- src/pages/ShippingInfo.jsx
+++ src/pages/ShippingInfo.jsx
@@ -1,13 +1,19 @@
-import ShippingInfoCont from '../components/ShippingInfoCont'
+import ShippingInfoHead from '../components/ShippingInfoHead'
+import ShippingInfoForm from '../components/ShippingInfoForm'
 import ItemCheckout from '../components/ItemCheckout'
 import shippingstyles from '../sass/pages/shippinginfo.module.scss'
 function ShippingInfo() {
   return (
     <div>
-        <div className={shippingstyles.shippingcontainer}>
-            <ShippingInfoCont/>
-            <ItemCheckout/>
+      <div className={shippingstyles.shippingcontainer}>
+        <div className={shippingstyles.infocont}>
+          <div>
+            <ShippingInfoHead/>
+            <ShippingInfoForm/>
+          </div>
         </div>
+        <ItemCheckout/>
+      </div>
     </div>
   )
 }
