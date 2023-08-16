010e6c72adfe20c4e6bdbb0b2d32176c9c3a1b15
Hameedarh <ajibadeolayimika@gmail.com>
2023-08-16 09:28:51 +0100

    chore:hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,59 +3,9 @@ import styles from '../sass/components/navbar.module.scss'
 import searchicon from '../assets/svg/search.svg'
 import profileicon from '../assets/svg/profile.svg'
 import carticon from '../assets/svg/cart.svg'
-import { slide as Menu } from 'react-burger-menu';
-function Navbar() {
-    
-    const hamStyles = {
-        bmBurgerButton: {
-          position: 'fixed',
-          width: '34px',
-          height: '22px',
-          right: '20px',
-          top: '20px',
-        },
-        bmBurgerBars: {
-            background: '#FFFFFF',
-          },
-          bmCrossButton: {
-            height: '24px',
-            width: '24px',
-          },
-          bmCross: {
-            background: '#FFFFFF',
-          },
-          bmMenuWrap: {
-            position: 'fixed',
-            height: '60%',
-            right: '0px',
-            width: '300px',
-            transform: 'translate3d(100%, 0px, 0px)',
-            transition: 'all 0.5s ease 0s',
-            top: '0px'
-          },
-          bmMenu: {
-            background: '#222222',
-            padding: '1em .5em 0',
-            fontSize: '1em',
-          },
-          bmMorphShape: {
-            fill: '#FFFFFF',
-          },
-          bmItemList: {
-            color: 'white',
-            padding: '0.8em',
-          },
-          bmItem: {
-            display: 'block',
-            textDecoration: 'none',
-            marginBottom: '10px',
-            color: 'white',
-          },
-          bmOverlay: {
-            display: 'none'
-          }
-        };
 
+function Navbar() {
+   
   return (
     <div className={styles.navBar}>
         <nav className='container'>
@@ -85,12 +35,18 @@ function Navbar() {
                     </div>
                 </div>
                 <div className={styles.navHamburger}>
-                <Menu styles={hamStyles}>
-                    <a className="menu-item" href="/">Home</a>
-                    <a className="menu-item" href="/shop">Shop</a>
-                    <a className="menu-item" href="/about">About</a>
-                    <a className="menu-item" href="/contact">Contact Us</a>
-                </Menu>
+                  <div className={styles.hamIndex}>
+                    <input type="checkbox" className={styles.resInp}/>
+                      <span className={styles.hamSpan}></span>
+                      <span className={styles.hamSpan}></span>
+                      <span className={styles.hamSpan}></span>
+                    <ul className={styles.hamburger}>
+                      <a href='#Home'><li>Home</li></a>
+                      <a href='#services'><li>Shop</li></a>
+                      <a href='#about'><li>About</li></a>
+                      <a href='#contact'><li>Contact</li></a>
+                    </ul>
+                  </div>
                 </div>
             </div>
         </nav>
